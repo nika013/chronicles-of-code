ee3a59f9a955ed2891849ea2a9c3ff12cad57026
nika013 <nkovz21@freeuni.edu.ge>
2024-04-08 20:38:52 +0400

    add basic tests for checking deletion of comments and spaces

--- front/src/tests/tokenizer.test.ts
+++ front/src/tests/tokenizer.test.ts
@@ -1,5 +1,6 @@
 import { Token } from "../Interpreter/Tokenizer/Token";
 import { TokenType } from "../Interpreter/Tokenizer/TokenType";
+import { Tokenizer } from "../Interpreter/Tokenizer/Tokenizer";
 
 describe('test Token', () => {
   test('test toString & constructor', () => {
@@ -27,3 +28,38 @@ describe('test Token', () => {
   })
 })
 
+describe('test tokenizer', () => {
+  test('test comments', () => {
+    let source = `
+    // first comment
+// second comment
+    ///////////////////
+    // daspojdaslkdnqwkndslmadls ///
+    `
+    let tokenizer = new Tokenizer(source)
+    expect(tokenizer.getSource()).toEqual("")
+  })
+
+  test('test spaces', () => {
+    let source = `
+
+              let name = 1
+
+    `
+    let tokenizer = new Tokenizer(source)
+    expect(tokenizer.getSource()).toEqual("letname=1")
+  })
+
+  test('test comments and spaces', () => {
+    let source = `
+// first comment
+        // second comment
+ ///////////////////
+    // daspojdaslkdnqwkndslmadls //
+  რიცხვი ერთი = 1
+
+` 
+    let tokenizer = new Tokenizer(source)
+    expect(tokenizer.getSource()).toEqual("რიცხვიერთი=1")
+  })
+})

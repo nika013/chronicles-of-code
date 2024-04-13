832aaf62eca0d90eb56241f7c489fc9c13f029c0
nika013 <nkovz21@freeuni.edu.ge>
2024-04-13 14:12:06 +0400

    Add visitor class

---
+++ front/src/Interpreter/AST/Statements/Interfaces/Visitor.ts
@@ -0,0 +1,11 @@
+import { FuncStatement } from "../ConcreteStatements/FuncStatement"
+import { IfStatement } from "../ConcreteStatements/IfStatement"
+import { VarStatement } from "../ConcreteStatements/VarStatement"
+import { WhileStatement } from "../ConcreteStatements/WhileStatement"
+
+export interface Visitor {
+    doWhileStatement(statement: WhileStatement): boolean
+    doIfStatement(statement: IfStatement): boolean
+    doFuncStatement(statement: FuncStatement): boolean
+    doVarStatement(statement: VarStatement): boolean
+}

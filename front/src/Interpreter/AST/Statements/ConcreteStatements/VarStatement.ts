0819f7c97bf593426367d504aad9fa2a3047e807
Nika Kovziridze <nkovziridze@bog.ge>
2024-05-04 17:48:48 +0400

    Add VarStatement Class

--- front/src/Interpreter/AST/Statements/ConcreteStatements/VarStatement.ts
+++ front/src/Interpreter/AST/Statements/ConcreteStatements/VarStatement.ts
@@ -1,13 +1,20 @@
 import { Statement } from "../Interfaces/Statement";
 import { Visitor } from "../Interfaces/Visitor";
-
+import { Variable } from "./VariableContainer";
 
 export class VarStatement implements Statement {
-    execute(): boolean {
-        return false;
-    }
     
-    accept(visitor: Visitor): void {
+    private variable: Variable
+
+    constructor(variable: Variable) {
+        this.variable = variable
+    }
+
+    public getVariable(): Variable {
+        return this.variable
+    }
+
+    public accept(visitor: Visitor): void {
         visitor.doVarStatement(this)
     }
 }

6348b79d55ae39980586b20d889487b65379bc91
Mariam Ratiani <mrati20@freeuni.edu.ge>
2024-05-05 11:34:20 +0400

    feat: moving background with character infinite length

--- front/src/game/scenes/ForestScene.ts
+++ front/src/game/scenes/ForestScene.ts
@@ -2,12 +2,13 @@ import {Scene} from "phaser";
 import Sprite = Phaser.GameObjects.Sprite;
 import Image = Phaser.GameObjects.Image;
 import {EventBus} from "../EventBus.ts";
+import TileSprite = Phaser.GameObjects.TileSprite;
 
 export class ForestScene extends Scene {
-    private background1: Image |Sprite | null;
-    private background2: Image| Sprite | null;
-    private background3: Image| Sprite | null;
-    private background4: Image| Sprite | null;
+    private background1: TileSprite;
+    private background2: TileSprite;
+    private background3: TileSprite;
+    private background4: TileSprite;
     private character: Phaser.GameObjects.Sprite;
     private ground: Sprite;
 
@@ -22,7 +23,7 @@ export class ForestScene extends Scene {
 
     }
 
-    calculateScale(obj: Sprite | Image, ) {
+    calculateScale(obj: Sprite | Image| TileSprite ) {
         const scaleX = this.cameras.main.width / obj.width
         const scaleY = this.cameras.main.height / obj.height
         return [scaleX, scaleY]
@@ -67,35 +68,51 @@ export class ForestScene extends Scene {
     }
     
     private createBackgrounds() {
-        this.background1 = this.add.image(0, 0, 'backgroundC1')
+        const backgroundHeight = this.cameras.main.height * 0.59
+        this.background1 = this.add.tileSprite(0, 0, this.cameras.main.width, backgroundHeight, 'backgroundC1')
         let scale = this.calculateScale(this.background1)
-        this.background1.setScale(scale[0] , scale[1]).setOrigin(0, 0).setScrollFactor(0)
+        this.background1.setScale(scale[0] , scale[1])
+            .setScrollFactor(0.2)   
+            .setOrigin(0, 0)
 
-        this.background2 = this.add.image(0, 0, 'backgroundC2')
+
+        this.background2 = this.add.tileSprite(0, 0, this.cameras.main.width,  backgroundHeight, 'backgroundC2')
         scale = this.calculateScale(this.background2)
-        this.background2.setScale(scale[0] , scale[1]).setOrigin(0, 0).setScrollFactor(0)
+        this.background2.setScale(scale[0] , scale[1])
+            .setOrigin(0, 0)
+            .setScrollFactor(0.4)
 
-        this.background3 = this.add.image(0, 0, 'backgroundC3')
+        this.background3 = this.add.tileSprite(0, 0,this.cameras.main.width, backgroundHeight,  'backgroundC3')
         scale = this.calculateScale(this.background3)
-        this.background3.setScale(scale[0] , scale[1]).setOrigin(0, 0).setScrollFactor(0)
+        this.background3.setScale(scale[0] , scale[1])
+            .setOrigin(0, 0)
+            .setScrollFactor(0.6)
 
-        this.background4 = this.add.image(0, 0, 'backgroundC4')
+        this.background4 = this.add.tileSprite(0, 0,this.cameras.main.width , backgroundHeight,  'backgroundC4')
         scale = this.calculateScale(this.background4)
-        this.background4.setScale(scale[0] , scale[1]).setOrigin(0, 0).setScrollFactor(0)
+        this.background4.setScale(scale[0] , scale[1])
+            .setOrigin(0, 0)
+            .setScrollFactor(0.8)
     }
 
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     create(_data: never) {
         this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x30ff00);
+        this.camera.setPosition(0, 0);
+        this.camera.setBounds(0, 0, this.game.config.width as number, this.game.config.height as number);
+
+        this.camera.setBackgroundColor(0x35ff00);
 
         this.platforms = this.physics.add.staticGroup()
 
         this.createBackgrounds()
         this.createGround()
         this.createBoy()
-        
+        this.physics.add.existing(this.character);
+        this.camera.startFollow(this.character, true, 0.1, 0.0);
+
+
         this.physics.add.collider(this.character, this.platforms);
 
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -136,10 +153,23 @@ export class ForestScene extends Scene {
     }
     
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    update(_time: never, _delta: never) {
+    update(_time: never, delta: never) {
         this.updateCharacterMovement()
-        
-        
+
+        if (this.character.body?.velocity.x !== 0) {  // Assuming character body exists and has velocity
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            this.background1.tilePositionX += 0.1 * this.character.body?.velocity.x / delta;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            this.background2.tilePositionX += 0.2 * this.character.body?.velocity.x / delta;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            this.background3.tilePositionX += 0.3 * this.character.body?.velocity.x / delta;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            this.background4.tilePositionX += 0.4 * this.character.body?.velocity.x / delta;
+        }
     }
 
     changeScene() {
